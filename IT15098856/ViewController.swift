c3600d16f8174dde34de8b9e9530916bfd56be00
Pandula Kalhara <pandulakalhara@pandulas-macbook-pro.local>
2018-10-01 12:15:24 +0530

    1st commit

---
+++ IT15098856/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IT15098856
+//
+//  Created by Pandula Kalhara on 10/1/18.
+//  Copyright © 2018 Pandula Kalhara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
